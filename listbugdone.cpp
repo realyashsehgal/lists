914f8944d4083992874b07f45bdd9963555b9f43
yash sehgal <155468916+realyashsehgal@users.noreply.github.com>
2024-04-04 18:56:12 +0530

    Update listbugdone.cpp
    did some bug fix now its all done with comments explaining each steps

--- listbugdone.cpp
+++ listbugdone.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <list>
 using namespace std;
+
+// Declare a global list named list1 to store integers
 list<int> list1;
+
+// Define a class named lists to perform operations on the list
 class lists
 {
 public:
-    int element;
-    int position;
+    int element;    // Store the element value
+    int position;   // Store the position value
 
 public:
+    // Function to push an element to the end of the list
     int push(int n)
     {
         cout << "Please enter the element you want to push" << endl;
         cin >> element;
-        list1.push_back(element);
+        list1.push_back(element);   // Add element to the end of the list
         n++;
         return n;
     }
+
+    // Function to pop the last element from the list
     int pop(int n)
     {
         if (n != 0)
         {
             cout << "Popping the last inserted element in the list " << endl;
-            list1.pop_back();
+            list1.pop_back();   // Remove the last element from the list
             cout << "Last element succesfully popped" << endl;
             n--;
             return n;
@@ -33,6 +40,8 @@ public:
             return n;
         }
     }
+
+    // Function to insert an element at a specific position in the list
     int push_specific(int n)
     {
         cout << "Please enter the position where you want to add the element" << endl;
@@ -44,11 +53,13 @@ public:
         {
             iter++;
         }
-        list1.insert(iter, element);
+        list1.insert(iter, element);    // Insert element at the specified position
         cout << "Succesfully inserted" << endl;
         n++;
         return n;
     }
+
+    // Function to remove an element from a specific position in the list
     int pop_specific(int n)
     {
         cout << "Please enter the postion you want to remove from the list" << endl;
@@ -60,7 +71,7 @@ public:
             {
                 it++;
             }
-            list1.erase(it);
+            list1.erase(it);    // Erase element at the specified position
             cout << "Element succesfully removed" << endl;
             n--;
             return n;
@@ -71,6 +82,8 @@ public:
             return n;
         }
     }
+
+    // Function to display the elements of the list
     void display(int n)
     {
         if (n != 0)
@@ -88,9 +101,11 @@ public:
         }
     }
 };
+
+// Main function to drive the program
 int main()
 {
-    lists mylist;
+    lists mylist;   // Create an object of class lists
     int i, n, elem, choice = 0;
     cout << "Please enter the number of elements you want in your list" << endl;
     cin >> n;
@@ -98,8 +113,9 @@ int main()
     {
         cout << "Please enter the element you want to push in the list " << endl;
         cin >> elem;
-        list1.push_back(elem);
+        list1.push_back(elem);   // Add elements to the global list
     }
+    // Display menu and perform operations based on user choice
     while (choice != 6)
     {
         cout << "---------------------------------------MENU---------------------------------------" << endl;
@@ -109,19 +125,19 @@ int main()
         switch (choice)
         {
         case 1:
-            n = mylist.push(n);
+            n = mylist.push(n); // Call push function
             break;
         case 2:
-            n = mylist.pop(n);
+            n = mylist.pop(n);  // Call pop function
             break;
         case 3:
-            n = mylist.push_specific(n);
+            n = mylist.push_specific(n);    // Call push_specific function
             break;
         case 4:
-            n = mylist.pop_specific(n);
+            n = mylist.pop_specific(n); // Call pop_specific function
             break;
         case 5:
-            mylist.display(n);
+            mylist.display(n);  // Call display function
             break;
         default:
             cout << "Invalid input please try again" << endl;
@@ -129,3 +145,4 @@ int main()
         }
     }
 }
+
